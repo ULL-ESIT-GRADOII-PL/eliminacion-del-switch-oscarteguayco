4a334d605b05f667cac26c848e9a5bb2da41a23f
alu0100825503 <alu0100825503@ull.edu.es>
2016-03-11 12:52:13 +0000

    Primera versión del constructor de Medida

--- assets/js/Medida.js
+++ assets/js/Medida.js
@@ -1,6 +1,25 @@
-var Medida = function() {
-    
-}
+var Medida = function(valor, tipo) {
+    var XRegExp = require('xregexp');
+    var xregexp = XRegExp('(?<numero> [0-9]{2} ) # valor \n' + 
+                         '(?<temp>  [a-zA-Z]+ ) # tipo    ');
+        
+    // TO-DO: arreglar XRegExp    
+    var regexp = /([0-9]{2})([a-zA-Z]+)/;    
+        
+    // Si no hay tipo, puede estar en 'valor'                   
+    if (!tipo) {  
+        var match = regexp.exec(valor);
+        console.log(match);
+        this.valor = match[1];
+        this.tipo = match[2];
+        
+    } else {
+        this.valor = valor;
+        this.tipo = tipo;
+    }
+};
+
+
 
 Medida.convertir = function(valor) {
   var measures = Medida.measures;
@@ -17,7 +36,7 @@ Medida.convertir = function(valor) {
       return source[target]().toFixed(2) + " "+target; // "0 Celsius"
     }
     catch(err) {
-      return 'Desconozco como convertir desde "'+tipo+'" hasta "'+destino+'"';
+      return 'Desconozco como convertir desde "' + tipo + '" hasta "' + destino + '"';
     }
   }
   else
